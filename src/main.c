9be11e2975ef34418fded707cb03aa026802e669
JANOTE Johann <jjohann@localhost.localdomain>
2019-04-23 14:10:08 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** NWP_mychap_2018
+** File description:
+** main.c
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+	printf("Testing CircleCI ...\n");
+	return (0);
+}
